74b038a7d0dcbeb77af55bb5d3c003f3d2cab772
hassan302-stack <kazmihassan336@gmail.com>
2022-01-24 23:45:49 +0500

    greeting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './index.css'
+
+
 
 function App() {
+  const d = new Date().getHours()
+  //var txt = " "
+  const [gt, setgt] = useState("Good Evening")
+  const [gc, setgc] = useState({})
+  useEffect(() => {
+    console.log("I am legend");
+    if (d > 0 && d < 12) {
+      setgt("Good Morning")
+      setgc({ color: "tomato" })
+    }
+    else if (d > 12 && d < 17) {
+      setgt("Good Afternoon")
+      setgc({ color: "green" })
+    }
+    else if (d > 17) {
+      setgt("Have A Good Night")
+      setgc({ color: "blue" })
+    }
+  }
+
+    , [])
+
+
+  console.log(d);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="con">
+        <h1 className="salutation" >Hello Sir, </h1>
+        <h1 className="greeting" style={gc} >{gt}</h1>
+      </div>
+    </>
   );
 }
-
 export default App;
